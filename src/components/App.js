b1fa4f9b72359d30146b3a858968fc497b8a558e
Sandeepraina40 <127074583+Sandeepraina40@users.noreply.github.com>
2025-10-04 14:36:55 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
+  // List of relatives you’re planning to visit
+  const relatives = [
+    "Uncle Raj",
+    "Aunt Meena",
+    "Grandparents",
+    "Cousin Rohan",
+    "Sister Priya"
+  ];
 
-export default App
+  return (
+    <div>
+      <h1>My Relatives for Diwali Visit 🎆</h1>
+
+      {/* Ordered list with key as relativeList */}
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li
+            key={`relativeListItem${index + 1}`}
+          >
+            {relative}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default App;
